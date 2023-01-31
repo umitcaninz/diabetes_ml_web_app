b2b44ca28013bd81705934a67b37b8b57ba17bce
Ümit Can İNÖZÜ <107713484+umitcaninz@users.noreply.github.com>
2023-02-01 02:48:41 +0300

    Add files via upload

---
+++ diabetes_prediction_web_app.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pickle
+import streamlit as st
+
+loaded_model = pickle.load(open("C:/Users/can/Desktop/deployy/trained_model.sav","rb"))
+
+
+
+def diabetes_prediction(input_data):
+    input_data_as_narray = np.asarray(input_data)
+    input_data_reshape = input_data_as_narray.reshape(1, -1)
+    prediction = loaded_model.predict(input_data_reshape)
+    print(prediction)
+
+
+    if (prediction[0] == 0) :
+        return 'Diyabet hastası değilsiniz'
+    else:
+        return 'Diyabet hastasısınız'
+
+def main():
+    st.title("Diyabet Hastalığı Tahmini")
+
+
+    Pregnancies = st.text_input("Hamilelik Sayısı")
+    Glucose = st.text_input("Glucose Değeri")
+    BloodPressure = st.text_input("Kan Basıncı Değeri")
+    SkinThickness = st.text_input("Cilt Kalınlığı Değeri")
+    Insulin = st.text_input("Insulin Değeri")
+    BMI = st.text_input("BMI Değeri")
+    DiabetesPedigreeFunction = st.text_input("Diabetes Pedigree Function Değeri")
+    Age = st.text_input("Yaş")
+
+    diagnosis = ""
+    if st.button("Diyabet Tahmin Sonucu"):
+        diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
+
+    st.success(diagnosis)
+
+
+if __name__ == '__main__':
+    main()
