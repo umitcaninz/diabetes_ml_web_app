962461c84a8012bead2f74df77aa35c3d06982f9
Ümit Can İNÖZÜ <107713484+umitcaninz@users.noreply.github.com>
2024-02-09 22:22:09 +0300

    Update diabetes_prediction_web_app.py

--- diabetes_prediction_web_app.py
+++ diabetes_prediction_web_app.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 import streamlit as st
 
-loaded_model = pickle.load(open("C:/Users/can/Desktop/deployy/trained_model.sav","rb"))
+loaded_model = pickle.load(open("trained_model.sav","rb"))
 
 
 
